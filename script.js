9e2bf3b5c0d71a7bb1206cb078d5a062d9fa28a8
Alex <spyhant@gmail.com>
2019-04-30 00:59:02 +0300

    Это наш первый комит git status
    git

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var adin = 1;
+var dva = 100;
+console.log(adin);
+console.log(dva);
+
+let persone = {
+    name:"Slon",
+    age:22,
+    isMarried: false
+};
+console.log(persone['name']);
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+let answer = confirm('You are love me?');
+console.log(answer);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
