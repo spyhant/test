e28c18583f7b95cd95dd9ce6546bae643264222a
Alex <spyhant@gmail.com>
2019-04-30 03:33:35 +0300

    Это мой четвёртый комит

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 'use strict';
 
-var money = prompt('Ваш бюджет на месяц?',"25000"),
-    time = prompt('Введите дату в формате YYYY-MM-DD',"30-04-2019");
-var income= [];
-var optionalExpenses ={};
-var oneQ = prompt('Введите обязательную статью расходов в этом месяце',"Еда"),
-    twoQ = prompt("Во сколько обойдется?",'15000');
+var money = prompt('Ваш бюджет на месяц?', "25000"),
+    time = prompt('Введите дату в формате YYYY-MM-DD', "30-04-2019");
+var income = [];
+var optionalExpenses = {};
+var oneQ = prompt('Введите обязательную статью расходов в этом месяце', "Еда"),
+    twoQ = prompt("Во сколько обойдется?", '15000');
 var expenses = {
-[oneQ] : twoQ
+    [oneQ]: twoQ
 };
 
 var appData = {
-    budget : (money),
-    DataTime : (time),
+    budget: (money),
+    DataTime: (time),
     expenses,
     optionalExpenses,
     income,
     savings: false
 };
 console.log(appData);
-alert(appData.budget/30);
+alert(appData.budget / 30);
